4b31c9e16a4d8616892c82ef5a24f2fc7aa7756d
Yogesh <45172882+yogeshxd@users.noreply.github.com>
2026-05-17 06:01:29 +0000

    Seprating architecture

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import face_recognition
+import cv2
+import numpy as np
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/analyze_face")
+async def analyze_face(file: UploadFile = File(...)):
+
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    locations = face_recognition.face_locations(rgb_img)
+    encodings = face_recognition.face_encodings(rgb_img, locations)
+
+    faces = []
+    for loc, enc in zip(locations, encodings):
+        faces.append({
+            "box": loc, # (top, right, bottom, left)
+            "encoding": enc.tolist() # Convert numpy array to standard list for JSON
+        })
+        
+    return {"faces": faces}
